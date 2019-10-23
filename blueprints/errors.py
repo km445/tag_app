39113ccfdbf5e4f1d6ada7840f0ad4e86072bfaa
barfvarf <barfvarf@gmail.com>
2019-10-23 09:40:21 +0300

    Updated errors

--- blueprints/errors.py
+++ blueprints/errors.py
@@ -12,22 +12,13 @@ errors = Blueprint('errors', __name__)
 response_data = {"data": None, "status": ResponseStatus.Error}
 
 
-@errors.app_errorhandler(404)
-def page_not_found(e):
+@errors.app_errorhandler(400)
+def bad_request(e):
     response_data["error"] = str(e)
     log.exception("Error for request => url: %s, data: %s, ip: %s, method: %s"
                   % (request.url, get_request_data(request),
                      request.remote_addr, request.method))
-    return jsonify(response_data), 404
-
-
-@errors.app_errorhandler(500)
-def internal_server_error(e):
-    response_data["error"] = str(e)
-    log.exception("Error for request => url: %s, data: %s, ip: %s, method: %s"
-                  % (request.url, get_request_data(request),
-                     request.remote_addr, request.method))
-    return jsonify(response_data), 500
+    return jsonify(response_data), 400
 
 
 @errors.app_errorhandler(403)
@@ -39,13 +30,31 @@ def access_denied(e):
     return jsonify(response_data), 403
 
 
-@errors.app_errorhandler(400)
-def bad_request(e):
+@errors.app_errorhandler(404)
+def page_not_found(e):
     response_data["error"] = str(e)
     log.exception("Error for request => url: %s, data: %s, ip: %s, method: %s"
                   % (request.url, get_request_data(request),
                      request.remote_addr, request.method))
-    return jsonify(response_data), 400
+    return jsonify(response_data), 404
+
+
+@errors.app_errorhandler(405)
+def method_not_allowed(e):
+    response_data["error"] = str(e)
+    log.exception("Error for request => url: %s, data: %s, ip: %s, method: %s"
+                  % (request.url, get_request_data(request),
+                     request.remote_addr, request.method))
+    return jsonify(response_data), 405
+
+
+@errors.app_errorhandler(500)
+def internal_server_error(e):
+    response_data["error"] = str(e)
+    log.exception("Error for request => url: %s, data: %s, ip: %s, method: %s"
+                  % (request.url, get_request_data(request),
+                     request.remote_addr, request.method))
+    return jsonify(response_data), 500
 
 
 @errors.app_errorhandler(Exception)
